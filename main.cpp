a24e0d6cb29d48219aa047033816ae74e082b92a
Thomas Stagg <thomstagg@gmail.com>
2023-09-24 20:05:43 +0200

    Testing branches in CLion part2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl; //This line was added in the Master branch
 
     std::cout << "Welcome to the TestBranch version" << std::endl; //This line was added
     return 0;
