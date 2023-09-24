bd9f4ae41893f227e2f62293f5a125adc5cda0dd
Thomas Stagg <thomstagg@gmail.com>
2023-09-24 20:20:35 +0200

    Testing branches in CLion part2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl; //This line was added in the Master branch
+    //This line was added in the Master branch
+    std::cout << "Hello, World!" << std::endl;
 
-    std::cout << "Welcome to the TestBranch version" << std::endl; //This line was added
+    //This line was added
+    std::cout << "Welcome to the TestBranch version" << std::endl;
 
+    //this is a comment to test rebasing
     std::cout << "What happens if my branch is older than the master branch?" << std::endl;
 
     return 0;
