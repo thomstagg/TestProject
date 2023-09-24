9a1b22e602eab618e26910c6ce7bb30e0f69ff4e
Thomas Stagg <thomstagg@gmail.com>
2023-09-24 19:57:55 +0200

    Testing branches in CLion part2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    std::cout << "Welcome to the TestBranch version" << std::endl;
+    std::cout << "Welcome to the TestBranch version" << std::endl; //This line was added
     return 0;
 }
